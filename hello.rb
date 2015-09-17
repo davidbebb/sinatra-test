c30b06a68faf773c60786e65fd6021222ddac5f7
davidbebb <davidbebb@gmail.com>
2015-09-17 19:04:51 +0100

    added get/post method

--- hello.rb
+++ hello.rb
@@ -9,3 +9,16 @@ end
 get '/secret' do
   'This is a not-very-secret page'
 end
+
+get '/' do
+  "Hello, World!"
+end
+
+get '/form' do
+  erb :form
+end
+
+post '/form' do
+  "You said '#{params[:coords]}'
+  and #{params[:orientation]}"
+end
